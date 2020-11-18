e7c03b8587c1c55efafbc7933ccbbdc580230f76
ＢＥＰＩＳ　ソ愛゜ <33198855+alrightalrightalrightalrightalright@users.noreply.github.com>
2020-11-18 16:03:08 +0300

    dökümantasyon eklendi, scrape düzenlemeleri yapıld backoff algoritamsını kullanan metot eklendi

--- Sahbndnİlan.py
+++ Sahbndnİlan.py
@@ -1,17 +1,17 @@
 #sahibinden mobil telefon ilanı
 class Shbndnİlan:
     def __init__(self,link='',fiyat=0,listingDate='',listingId=0,brand=0,model="",os=0,
-    storageCap=0,screenSize=0,ramSize=0,cameraRes=0,frontCamRes=0,color=0,
+    storageCap=0,screenSize=0,ram=0,cameraRes=0,frontCamRes=0,color=0,
     warrantyStatus=0,fromWho=0,title="",city="",town=""):
         self.link=link
-        self.fiyat=fiyat
+        self.price=fiyat
         self.listingDate=listingDate
         self.listingId=listingId
         self.brand=brand
         self.os=os
         self.storageCap=storageCap#internal storage, gb
         self.screenSize=screenSize
-        self.ramSize=ramSize
+        self.ram=ram    #MB
         self.cameraRes=cameraRes
         self.frontCamRes=frontCamRes
         self.color=color
@@ -21,13 +21,14 @@ class Shbndnİlan:
         self.town=town
         self.title=title
         self.model=model
+        
 
     def print(self):
         print('link: {}\n{}₺, {} {} {} \n%{} gb storage, {} inch screen size, {} ram\n{}mb main cam, {}mb front cam, {} color \n{} of warranty, from: {}\n {} {}{} '
         '\n---------------------------\n'
-        .format(self.link, self.fiyat  , self.brand ,self.model, self.os, self.storageCap, self.screenSize,
-        self.ramSize, self.cameraRes, self.frontCamRes,  self.color, self.warrantyStatus, self.fromWho,
+        .format(self.link, self.price  , self.brand ,self.model, self.os, self.storageCap, self.screenSize,
+        self.ram, self.cameraRes, self.frontCamRes,  self.color, self.warrantyStatus, self.fromWho,
         self.city,self.town, self.listingDate))
     def getCSV(self):
-        return ",".join(str(x) for x in(self.link,self.fiyat,self.listingDate,self.listingId,self.brand,self.os,self.storageCap,self.screenSize,self.ramSize,self.cameraRes,self.frontCamRes,self.color,self.warrantyStatus,self.fromWho,self.city,self.town,self.title,self.model))
+        return ",".join(str(x) for x in(self.link,self.price,self.listingDate,self.listingId,self.brand,self.os,self.storageCap,self.screenSize,self.ram,self.cameraRes,self.frontCamRes,self.color,self.warrantyStatus,self.fromWho,self.city,self.town,self.title,self.model))
  
--- Scraper.py
+++ Scraper.py
@@ -6,6 +6,7 @@ import sys
 import _thread
 from io import StringIO
 import Firebase
+import backoff
 #utils
 def stdoutToStr(ilan):
     old_stdout = sys.stdout
@@ -19,11 +20,11 @@ def stdoutToStr(ilan):
 
     return output
 
-
 ### Request Queries ###
 lastXdays = lambda a : "date="+a + "days" # date=30days
 getOffset = lambda pageNumb: "pagingOffset=" + str(pageNumb * 50)  #pagingOffset=950
 ###
+
 class Scraper:
     #Request kullanımı: host + relative url + ek queries
     baseUrl="https://www.sahibinden.com" #url="https://www.sahibinden.com/cep-telefonu-modeller/ikinci-el?pagingSize=50"
@@ -32,13 +33,39 @@ class Scraper:
     headers = { 
         'Accept': '*/*',
         'Host': 'www.sahibinden.com',      
-        'Connection': 'keep-alive', 
+        'Connection': 'closed', 
         'X-Requested-With':'XMLHttpRequest',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36 OPR/71.0.3770.317'
+        'User-Agent': 'Mmdghgilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36 OPR/71.0.3770.317'
         }
     frbs=Firebase.Firebase()
     
+    def _scrapeListing(self,soup, e):
+        """Smallest scraping work, scrapes all the listings in the web page and returns its data as object
+        :param soup: bs4 soup of webpage that contains many listings.
+        :param e: each listing element. some data scraped from breadcrumb bar thats why there is also soup param."""
+        id= e.get("data-id")    #listing id
+        listingUrl = e.find('td',class_="searchResultsLargeThumbnail").find("a").get("href") #link
+        title=e.find('td',class_="searchResultsLargeThumbnail").find("a").get("title") #title
 
+        #yaprak üzerinde scrape
+        if len(e.find_all('td',class_="searchResultsTagAttributeValue")) ==0: #model üzerinden scrape yapılıyorsa: ...>apple>iphone 3g
+            model =soup.find("li",class_="breadcrumbItem leaf").span.text
+            brand=soup.find("li",class_="breadcrumbItem leaf").find_previous("li").span.text
+        else:
+            brand =soup.find("li",class_="breadcrumbItem leaf").span.text
+            model = e.find_all('td',class_="searchResultsTagAttributeValue")[0].get_text()  #model: iphone 7 plus
+
+        price= e.find('td',class_="searchResultsPriceValue").div.get_text()    #price
+
+        date1= e.find('td',class_="searchResultsDateValue").find_all("span")[0].get_text() #4 kasım
+        date2=e.find('td',class_="searchResultsDateValue").find_all("span")[1].get_text() #2020
+        for j,text in enumerate(e.find('td',class_="searchResultsLocationValue").stripped_strings): #ilki il, ikincisi ilçe
+            city= text if j == 0 else "null"
+            town = text if j==1  else "null"
+
+        ilan= Sahbndnİlan.Shbndnİlan(listingUrl,price,date1+" "+date2,id,brand,model,title=title,city=city,town=town)
+        #ilan.print()
+        return ilan
 
     def __init__(self):
         s=self.s = requests.Session()
@@ -50,8 +77,16 @@ class Scraper:
         self.pageCount= int(soup.find("p", class_="mbdef").text.split()[1].replace(".",""))
         self.listingCount=int(soup.find("div", class_="result-text").find_all("span")[1].text.split()[0].replace(".",""))
         
-
+    @backoff.on_exception(backoff.expo,
+                      requests.exceptions.RequestException)
     def crawl(self, soup,level=0):
+        """Scrapes whole mobile phone listings in website. So that website has
+        multiple categories with sub-categories itself, a recursive scraping method 
+        that works like a crawler is required to do this job.  
+
+        :param soup: bs4 soup of the webpage with all the main categories itself.
+        :param level: recursion level.
+        """
         listingCount=int(soup.find("div", class_="result-text").find_all("span")[1].text.split()[0].replace(".",""))
         if listingCount<=1000 or len(soup.find("div",id="searchCategoryContainer").find_all("li"))==0 :
             self.scrape(soup,level)
@@ -67,10 +102,14 @@ class Scraper:
             self.crawl(soup,level+1)
             print(level*"\t"+ 'crawling done for: ', phoneModel.text.replace("\n",""))
 
-    #FileIO.saveResponse("bepsi.html",r.text)
 
     def scrape(self, soup,level=0): 
-             
+        """Does the scraping job where the webpage has listings.
+
+        :param soup: bs4 soup of the webpage to be scraped.
+        :param level: since the scraping process used in crawling, level specifies the recursion level 
+        of the crawling process. it is used in printing or visualising.
+        """
         listingCount=int(soup.find("div", class_="result-text").find_all("span")[1].text.split()[0].replace(".",""))
         pageCount= 20 if int(listingCount)//50 >=20 else int(listingCount)//50 #çünkü az ilanlı kategoride sonraki sayfa butonları yok      
         i=0
@@ -81,71 +120,60 @@ class Scraper:
             i+=1
             soup = BeautifulSoup(r.text, 'html.parser')
             #nextPageUrl=soup.find(lambda btn:btn.name=="a" and "Sonraki" in btn.text).get("href")
-            #print(nextPageUrl)
             
             links= soup.find_all("tr")
             
-            ###TODO: find count of listings
-            #listings=(x for x in links if x.has_attr("data-id"))
-            #count= sum(1 for x in listings)
-            #print(count)
             file=open("ekmek.txt","a", encoding="utf8")
             #e: her bir ilan elementi, data-id attr'sine sahip her bir <tr>
             for e in links:
                 if e.has_attr("data-id") is not True: continue
-                id= e.get("data-id")    #listing id
-                listingUrl = e.find('td',class_="searchResultsLargeThumbnail").find("a").get("href") #link
-                title=e.find('td',class_="searchResultsLargeThumbnail").find("a").get("title") #title
-
-                #yaprak üzerinde scrape
-                if len(e.find_all('td',class_="searchResultsTagAttributeValue")) ==0: #model üzerinden scrape yapılıyorsa: ...>apple>iphone 3g
-                    model =soup.find("li",class_="breadcrumbItem leaf").span.text
-                    brand=soup.find("li",class_="breadcrumbItem leaf").find_previous("li").span.text
-                else:
-                    brand =soup.find("li",class_="breadcrumbItem leaf").span.text
-                    model = e.find_all('td',class_="searchResultsTagAttributeValue")[0].get_text()  #model: iphone 7 plus
-
-                price= e.find('td',class_="searchResultsPriceValue").div.get_text()    #price
-
-                date1= e.find('td',class_="searchResultsDateValue").find_all("span")[0].get_text() #4 kasım
-                date2=e.find('td',class_="searchResultsDateValue").find_all("span")[1].get_text() #2020
-                for j,text in enumerate(e.find('td',class_="searchResultsLocationValue").stripped_strings): #ilki il, ikincisi ilçe
-                    city= text if j == 0 else "null"
-                    town = text if j==1  else "null"
-
-                ilan= Sahbndnİlan.Shbndnİlan(listingUrl,price,date1+" "+date2,id,brand,model,title=title,city=city,town=town)
-                #ilan.print()
-                self.frbs.AddListing(ilan)
-                file.write(ilan.listingId+"\n")
+                ilan=self._scrapeListing(soup,e)
+                self.deepScrape(ilan)
+                ilan.print()
+                #self.frbs.AddListing(ilan)
+                #file.write(ilan.listingId+"\n")
+                file.write(ilan.getCSV()+"\n")
                 
             file.close()  
 
-    def deepScrape(self,id):
-        r = self.s.get(self.baseUrl+self.listingUrl+id+ "/detay",headers=self.headers, timeout=None)
+    def deepScrape(self,ilan):
+        """Scrapes a listing form its listing page. it requests the listing with id parameter
+        then scrapes its data.
+        :param id: listing id"""
+        r = self.s.get(self.baseUrl+self.listingUrl+ilan.listingId+ "/detay",headers=self.headers, timeout=None)
         print("page: ",r.status_code)  
         soup = BeautifulSoup(r.text, 'html.parser')
         info= soup.find("div",class_="classifiedInfo")
 
         infoList=info.find("ul",class_="classifiedInfoList").find_all("li")
-        price=info.find("h3").text.split(" ")[17] #HARDCODED. !!!
-        location="".join(info.find("h2").text.split())
-        
-        listingDate=" ".join(infoList[1].span.text.split(" ")[16:])
-        brand=infoList[2].span.text.replace("\xa0","")
-        model=infoList[3].span.text.replace("\xa0","")
-        os=infoList[4].span.text.split("\t")[1]
-        internalMem=infoList[5].text.split("\t")[1].split()[0]
-        screenSize=infoList[6].text.split()[2]
-        ramMem=infoList[7].text.split()[2]#gb#'501 MB - 1 GB\n' wtf
-        camRes=infoList[8].text.split()[1]#mp
-        frontCamRes=infoList[9].text.split()[2]
-        color=infoList[10].text.split()[1]
-        guarantee=" ".join(infoList[11].text.split()[1:])
-        seller=infoList[12].text.split()[1]
-        usedStatus=" ".join(infoList[14].text.split()[1:])
+        ilan.price= info.find("h3").text.split(" ")[17] #HARDCODED. !!!
+        ilan.city="".join(info.find("h2").text.split()[0])
+        ilan.town="".join(info.find("h2").text.split()[2])
+        ilan.listingDate =" ".join(infoList[1].span.text.split(" ")[16:])
+        ilan.brand=infoList[2].span.text.replace("\xa0","")
+        ilan.model=infoList[3].span.text.replace("\xa0","")
+        ilan.os=infoList[4].span.text.split("\t")[1]
+
+        #TODO: fix interval values like 501mb-1gb
+        ilan.internalMem=infoList[5].text.split("\t")[1].split()[0]
+        ilan.screenSize=infoList[6].text.split()[2]
+
+        #4 durum: 15gb, 256mb, 510mb- 2 gb, yok
+        ramText=" ".join(infoList[7].text.split())#kamera 1mb ve altı
+        ramUnitMultiplier=1 if ramText.split()[2]=="Yok" or ramText.split()[3]=="MB" else  1024
+        ramSize= 0 if ramText.find("Yok") != -1 else int(ramText.split()[2])*ramUnitMultiplier
+        ilan.ram= ramSize
+
+        ilan.cameraRes=infoList[8].text.split()[1]#mp
+        ilan.frontCamRes=infoList[9].text.split()[2]
+        ilan.color=infoList[10].text.split()[1]
+        ilan.warrantyStatus=" ".join(infoList[11].text.split()[1:])
+        ilan.fromWho=infoList[12].text.split()[1]
+        #ilan.usedStatus=" ".join(infoList[14].text.split()[1:]) no need
 
 
         x=2
+        print(x)
         
 
     def lightScrape(self):
@@ -154,13 +182,8 @@ class Scraper:
 
 
 bepsi=Scraper()
+
 bepsi.crawl(bepsi.soup)
-
-
-file=open("ekmek.txt","r")
-laynsss=file.readlines()
-for ln in laynsss:
-    bepsi.deepScrape(ln.replace("\n",""))
 
 
 
